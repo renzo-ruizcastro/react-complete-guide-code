ef7c0720fb9dd3f2abb0ceacc1b77075d2d2ac66
renzo-ruizcastro <84095708+renzo-ruizcastro@users.noreply.github.com>
2022-01-12 14:07:40 -0500

    styled components done

--- code-me/src/components/UI/Button/Button.js
+++ code-me/src/components/UI/Button/Button.js
@@ -1,13 +1,43 @@
 import React from 'react';
+// import './Button.css';
+// Using styled-components
+import styled from 'styled-components';
 
-import './Button.css';
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+// Using styled-components
+// tagged template literal
+const Button = styled.button`
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    /* Styles when the condition is met */
+    width: auto;
+  }
+
+  /*Pseudo selectors: preceded by &*/
+  &:focus {
+    outline: none;
+  }
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
