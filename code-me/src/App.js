5132b7c274be75b5ebba392f1f6bb662561578e2
renzo-ruizcastro <84095708+renzo-ruizcastro@users.noreply.github.com>
2022-02-18 12:32:03 -0500

    Learning useEffect basics

--- code-me/src/App.js
+++ code-me/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,14 +7,32 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(
+    // 1st: It's a function, not necessarily an anonymus function
+    () => {
+      // The code here could be performance expensive
+      const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+      if (storedUserLoggedInInformation === '1') {
+        // triggers a re-evaluation of the component, but here useEffect won't be called again because its arguments (none) have not changed
+        setIsLoggedIn(true);
+      }
+    },
+    // 2nd: It's an array of dependencies, if the dependencies change, the function will be re-run
+    // If the array of dependencies is empty, the function will be run only once when the component is mounted
+    []
+  );
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    // the value stored is a string
+    localStorage.setItem('isLoggedIn', '1');
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
--- code-me/src/components/Login/Login.js
+++ code-me/src/components/Login/Login.js
@@ -1,30 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
-const Login = (props) => {
+const Login = props => {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+  useEffect(
+    () => {
+      // save code, one place
+      // validation is still a side effect
+      // whenever you have an action that should be executed in response to another action could be considered as a side effect
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    },
+    // add as dependencies the variables you use in the effect function that aren't only scoped to it
+    // state updating functions are ensured by React to never change, so you can prescinde of them
+    [enteredEmail, enteredPassword]
+  );
 
+  const emailChangeHandler = event => {
+    setEnteredEmail(event.target.value);
+    /*
     setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6
     );
+    */
   };
 
-  const passwordChangeHandler = (event) => {
+  const passwordChangeHandler = event => {
     setEnteredPassword(event.target.value);
-
+    /*
     setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6
     );
+    */
   };
 
   const validateEmailHandler = () => {
@@ -35,7 +51,7 @@ const Login = (props) => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
-  const submitHandler = (event) => {
+  const submitHandler = event => {
     event.preventDefault();
     props.onLogin(enteredEmail, enteredPassword);
   };
