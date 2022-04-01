8aba063b1bbacd51682fb57c3ef15103f77ce5fc
renzo-ruizcastro <84095708+renzo-ruizcastro@users.noreply.github.com>
2022-04-01 14:08:17 -0500

    Rendering meal items without form

---
+++ code-me/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from 'react';
+// Supported by React
+import styles from './Header.module.css';
+// import images
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = props => {
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles['main-image']}>
+        <img src={mealsImage} alt="A table full of delicious food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ code-me/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,5 @@
+import styles from './MealItemForm.module.css';
+
+const MealItemForm = props => {};
+
+export default MealItemForm;
