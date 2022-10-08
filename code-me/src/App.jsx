f6acff9789919a148f03f19e50128cd0a0e6cd88
Renzo Ruiz <renzo.ruizcastro@hotmail.com>
2022-10-07 19:38:34 -0500

    20: Basics completed

--- code-me/src/App.jsx
+++ code-me/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import MainHeader from './components/MainHeader';
 import Welcome from './pages/Welcome';
@@ -12,17 +12,26 @@ function App() {
       {/* <h2>Let's get started!</h2> */}
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        {/* productId is a dynamic path segment */}
-        {/* productId is a name up to you */}
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
+        {/* Switch only shows in screen one route page */}
+        <Switch>
+          {/* exact here matters because every path starts with '/' */}
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          {/* Matching paths in router means, starting with the same path */}
+          {/* Exact prop requires the full path to match with the given one */}
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          {/* productId is a dynamic path segment */}
+          {/* productId is a name up to you */}
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
--- code-me/src/pages/Welcome.jsx
+++ code-me/src/pages/Welcome.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { Route } from 'react-router-dom';
 
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <>
+      <h1>The Welcome Page</h1>
+      {/* If the page is active the route will be evaluated */}
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user!</p>
+      </Route>
+    </>
+  );
 };
 
 export default Welcome;
