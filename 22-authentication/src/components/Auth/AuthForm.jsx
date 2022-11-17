7efbfac0ff43a959b89a29b1be0d5a427d518e07
Renzo Ruiz <renzo.ruizcastro@hotmail.com>
2022-11-16 22:06:44 -0500

    feat: connection to firebase, feedback to user

--- 22-authentication/src/components/Auth/AuthForm.jsx
+++ 22-authentication/src/components/Auth/AuthForm.jsx
@@ -1,30 +1,92 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
+const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const API_SINGIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+const API_SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
+    setIsLogin(prevState => !prevState);
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // optional: Add validation
+
+    setIsLoading(true);
+    const url = isLogin ? API_SINGIN : API_SIGNUP;
+    const body = {
+      email: enteredEmail,
+      password: enteredPassword,
+      returnSecureToken: true,
+    };
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          // Firebase password must be at least 6 characters long
+          return res.json().then(data => {
+            // show error modal
+            let errorMessage = 'Authentication failed!';
+            // Checking firebase error structure
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        alert(err.message);
+      });
   };
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input ref={emailInputRef} type="email" id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            ref={passwordInputRef}
+            type="password"
+            id="password"
+            required
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading ? (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          ) : (
+            <p>Sending request...</p>
+          )}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
